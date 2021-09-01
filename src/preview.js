5d822f3822e0576777b4f0ed0ab0aefd5ae26a1c
Suning Yao <sy2940@nyu.edu>
2021-09-01 17:25:22 -0400

    ✨ add code syntax highlighting

--- src/preview.js
+++ src/preview.js
@@ -3,13 +3,29 @@ import { unified } from 'unified'
 import remarkParse from 'remark-parse/lib'
 import remarkGfm from 'remark-gfm'
 import remarkReact from 'remark-react/lib'
+import RemarkCode from './remark-code'
+import { defaultSchema } from 'hast-util-sanitize'
 import 'github-markdown-css'
+
+const schema = {
+    ...defaultSchema,
+    attributes: {
+      ...defaultSchema.attributes,
+      code: [...(defaultSchema.attributes?.code || []), 'className']
+    }
+  }
 
 const Preview = (props) => {
     const markdown = unified()
         .use(remarkParse)
         .use(remarkGfm)
-        .use(remarkReact, React)
+        .use(remarkReact, {
+            createElement: React.createElement,
+            sanitize: schema,
+            remarkReactComponents: {
+              code: RemarkCode
+            }
+          })
         .processSync(props.doc).result
 
     return (
